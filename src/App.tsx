45ea8c5fb790535313bb65304612b13a12ae1d98
swlee <swlee@whatap.io>
2022-01-16 15:06:02 +0900

    fix(app): router baseurl

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import store from "src/store";
+import store from 'src/store';
 
-import IndexPage from "./pages/IndexPage";
-import AutomationPage from "./pages/AutomationPage";
+import IndexPage from './pages/IndexPage';
+import AutomationPage from './pages/AutomationPage';
 
 let persistor = persistStore(store);
 
 const App = () => (
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={null}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact>
             <IndexPage />
           </Route>
-          <Route path="/auto">
+          <Route path='/auto'>
             <AutomationPage />
           </Route>
         </Switch>
